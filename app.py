6397dae62872356ccef39644d59f062c9b6d4440
John MacTavish <john@techsolx.com>
2024-11-05 15:33:37 -0500

    fix(): re-configure code for VTPUG examples.

--- app.py
+++ app.py
@@ -2,43 +2,44 @@
 
 import random
 
-responses = ["It is certain.",
-             "It is decidedly so.",
-             "Without a doubt.",
-             "Yes - definitely.",
-             "You may rely on it.",
-             "As I see it, yes.",
-             "Most likely.",
-             "Outlook good.",
-             "Yes.",
-             "Signs point to yes.",
-             "Reply hazy, try again.",
-             "Ask again later.",
-             "Better not tell you now.",
-             "Cannot predict now.",
-             "Concentrate and ask again.",
-             "Don't count on it.",
-             "My reply is no.",
-             "My sources say no.",
-             "Outlook not so good.",
-             "Very doubtful.",
-             ]
+responses = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+]
 
 result_num = random.randint(0, 19)
 
 
-def ask_question(answer):
+def ask_question():
     question = input("What is your question? ")
     if "?" in question:
-        return response
+        return True
     else:
         print("Please phrase your question in the form of a question - ?")
 
 
+answer = responses[result_num]
 response = ask_question()
-answer = responses[result_num])
-ask_question(answer)
+
 if response:
-    print(response)
+    print(answer)
 else:
     ask_question()
